3c7df628d1ba781bacd461833a33d9be9e6d452f
Nishanthumesh07 <nishanthumesh07@gmail.com>
2025-07-09 14:00:07 +0530

    Updated app.py with fixes

--- app.py
+++ app.py
@@ -145,5 +145,3 @@ def index():
         return render_template('index.html', uploaded_image=result_path)
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
